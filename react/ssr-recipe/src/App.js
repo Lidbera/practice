224a0c353041d46fcc958d3ed79e609460e87c6a
lectinua <lidbera@naver.com>
2020-08-19 15:17:02 +0900

    ~20

--- react/ssr-recipe/src/App.js
+++ react/ssr-recipe/src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
 import Menu from "./components/Menu";
 import { Route } from "react-router-dom";
-import BluePage from "./pages/BluePage";
-import RedPage from "./pages/RedPage";
+
+import loadable from "@loadable/component";
+const RedPage = loadable(() => import("./pages/RedPage"));
+const BluePage = loadable(() => import("./pages/BluePage"));
+const UsersPage = loadable(() => import("./pages/UsersPage"));
 
 function App() {
   return (
@@ -11,6 +14,7 @@ function App() {
       <hr />
       <Route path="/red" component={RedPage} />
       <Route path="/blue" component={BluePage} />
+      <Route path="/users" component={UsersPage} />
     </div>
   );
 }
--- react/ssr-recipe/src/index.server.js
+++ react/ssr-recipe/src/index.server.js
@@ -5,18 +5,17 @@ import { StaticRouter } from "react-router-dom";
 import App from "./App";
 import path from "path";
 import fs from "fs";
+import rootReducer, { rootSaga } from "./modules";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+import PreloadContext from "./lib/PreloadContext";
+import createSagaMiddleware, { END } from "redux-saga";
+import { ChunkExtractor, ChunkExtractorManager } from "@loadable/server";
 
-// asset-manifest.json에서 파일 경로들을 조회함
-const manifest = JSON.parse(
-  fs.readFileSync(path.resolve("./build/asset-manifest.json"), "utf8")
-);
+const statsFile = path.resolve("./build/loadable-stats.json");
 
-const chunks = Object.keys(manifest.files)
-  .filter((key) => /chunk\.js$/.exec(key)) // chunk.js로 끝나는 키를 찾아서
-  .map((key) => `<script src="${manifest.files[key]}"></script>`) // 스크립트 태그로 변환하고
-  .join(""); // 합침
-
-function createPage(root) {
+function createPage(root, tags) {
   return `<!DOCTYPE html>
   <html lang="en">
   <head>
@@ -25,16 +24,15 @@ function createPage(root) {
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
     <meta name="theme-color" content="#000000"/>
     <title>React App</title>
-    <link href="${manifest.files["main.css"]}" rel="stylesheet"/>
+    ${tags.styles}
+    ${tags.links}
   </head>
   <body>
     <noscript>You need to enable Javascript to run this app.</noscript>
     <div id="root">
       ${root}
     </div>
-    <script src="${manifest.files["runtime-main.js"]}"></script>
-    ${chunks}
-    <script src="${manifest.files["main.js"]}></script>
+    ${tags.scripts}
   </body>
   </html>`;
 }
@@ -42,17 +40,59 @@ function createPage(root) {
 const app = express();
 
 // 서버 사이드 렌더링을 처리할 핸들러 함수
-const serverRender = (req, res, next) => {
+const serverRender = async (req, res, next) => {
   // 이 함수는 404가 더야 하는 상황에 404를 띄우지 않고 서버 사이드 렌더링을 해 줌
 
   const context = {};
-  const jsx = (
-    <StaticRouter location={req.url} context={context}>
-      <App />
-    </StaticRouter>
+  const sagaMiddleware = createSagaMiddleware();
+
+  const store = createStore(
+    rootReducer,
+    applyMiddleware(thunk, sagaMiddleware)
   );
+  const sagaPromise = sagaMiddleware.run(rootSaga).toPromise();
+
+  const preloadContext = {
+    done: false,
+    promises: [],
+  };
+
+  const extractor = new ChunkExtractor({ statsFile });
+
+  const jsx = (
+    <ChunkExtractorManager extractor={extractor}>
+      <PreloadContext.Provider value={preloadContext}>
+        <Provider store={store}>
+          <StaticRouter location={req.url} context={context}>
+            <App />
+          </StaticRouter>
+        </Provider>
+      </PreloadContext.Provider>
+    </ChunkExtractorManager>
+  );
+  ReactDOMServer.renderToStaticMarkup(jsx); // renderToStaticMarkup으로 한번 렌더링함
+  store.dispatch(END); // redux-saga의 END 액션을 발생시키면 액션을 모니터링하는 사가들이 모두 종료됨
+  try {
+    await sagaPromise; // 기존에 진행 중이던 사가들이 모두 끝날 때까지 기다림
+    await Promise.all(preloadContext.promises); // 모든 프로미스를 기다림
+  } catch (e) {
+    return res.status(500);
+  }
+  preloadContext.done = true;
   const root = ReactDOMServer.renderToString(jsx); // 렌더링을 하고
-  res.send(createPage(root)); // 클라이언트에게 결과물을 응답함
+  // JSON을 문자열로 변환하고 악성 스크립트가 실행되는 것을 방지하기 위해 <를 치환 처리
+  // https://reddux.js.org/recipes/server-rendering#security-considerations
+  const stateString = JSON.stringify(store.getState()).replace(/</g, "\\u003c");
+  const stateScript = `<script>__PRELOADED_STATE__=${stateString}</script>`; // 리덕스 초기 상태를 스크립트로 주입
+
+  // 미리 불러와야 하는 스타일/스크립트를 추출
+  const tags = {
+    scripts: stateScript + extractor.getScriptTags(), // 스크립트 파부분에 리덕스 상태 넣기
+    links: extractor.getLinkTags(),
+    styles: extractor.getStyleTags(),
+  };
+
+  res.send(createPage(root, tags)); // 클라이언트에게 결과물을 응답함
 };
 
 const serve = express.static(path.resolve("./build"), {
