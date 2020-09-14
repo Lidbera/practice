2d2232390f24977b6c318ee6a33dcff61ff4ba16
Lidbera <lidbera@naver.com>
2020-09-14 21:39:16 +0900

    temp

--- react/blog/blog-frontend/src/index.js
+++ react/blog/blog-frontend/src/index.js
@@ -9,13 +9,28 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import { Provider } from 'react-redux';
 import createSagaMiddleware from 'redux-saga';
 import { createStore, applyMiddleware } from 'redux';
+import { tempSetUser, check } from './modules/user';
 
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(
   rootReducer,
   composeWithDevTools(applyMiddleware(sagaMiddleware)),
 );
+
+function loadUser() {
+  try {
+    const user = localStorage.getItem('user');
+    if (!user) return; // 로그인 상태가 아니라면 아무 것도 안함
+
+    store.dispatch(tempSetUser(user));
+    store.dispatch(check());
+  } catch (e) {
+    console.log('localStorage is not work');
+  }
+}
+
 sagaMiddleware.run(rootSaga);
+loadUser();
 
 ReactDOM.render(
   <Provider store={store}>
