b6cc228572cc107c8a54ea5a9cccc4bd99dcd2a7
lectinua <lidbera@naver.com>
2020-09-24 18:03:44 +0900

    servletContainer

---
+++ springboot/servletContainer/src/main/java/com/lect/PortListener.java
@@ -0,0 +1,15 @@
+package com.lect;
+
+import org.springframework.boot.web.servlet.context.ServletWebServerApplicationContext;
+import org.springframework.boot.web.servlet.context.ServletWebServerInitializedEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PortListener implements ApplicationListener<ServletWebServerInitializedEvent> {
+    @Override
+    public void onApplicationEvent(ServletWebServerInitializedEvent event) {
+        ServletWebServerApplicationContext context = event.getApplicationContext();
+        System.out.println(">>> port: " + context.getWebServer().getPort());
+    }
+}
---
+++ springboot/servletContainer/src/main/java/com/lect/TestController.java
@@ -0,0 +1,14 @@
+package com.lect;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+    @ResponseBody
+    @GetMapping
+    public String hello() {
+        return "Hello Spring";
+    }
+}
